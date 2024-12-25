4d2aeca705d123ac7a399060a2150a7d43d0b0d5
Namia-X <mk258855@outlook.com>
2024-12-25 18:02:48 +0800

    更新 diy-part.sh

--- diy-part.sh
+++ diy-part.sh
@@ -56,7 +56,7 @@ git clone --depth=1 https://github.com/sirpdboy/luci-app-netdata package/luci-ap
 git_sparse_clone dev https://github.com/vernesong/OpenClash luci-app-openclash
 git_sparse_clone main https://github.com/morytyann/OpenWrt-mihomo luci-app-mihomo mihomo
 git clone --depth=1 https://github.com/immortalwrt/homeproxy package/luci-app-homeproxy
-git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
+# git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
